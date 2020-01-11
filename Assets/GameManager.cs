b09b55b6bfe936092c887a47d82c327573a27e0f
hyd3r <hyd3r.exe@gmail.com>
2020-01-11 20:00:24 +0800

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float scrollSpeed = 5f;
+    public GameObject[] obstacles;
+    public float frequency = 0.5f;
+    float counter = 0f;
+    public Transform spawnPoint;
+    void Start()
+    {
+        generateObstacles();
+    }
+
+    void Update()
+    {
+        if (counter <= 0f)
+        {
+            generateObstacles();
+        }
+        else
+        {
+            counter -= Time.deltaTime * frequency;
+        }
+        GameObject obstacle;
+        for(int i=0; i < transform.childCount; i++)
+        {
+            obstacle = transform.GetChild(i).gameObject;
+            scroll(obstacle);
+            if (obstacle.transform.position.x <= -15f) Destroy(obstacle);
+        }
+    }
+
+    void scroll(GameObject obstacle)
+    {
+        obstacle.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
+    void generateObstacles()
+    {
+        GameObject newObstacle = Instantiate(obstacles[Random.Range(0, obstacles.Length)], spawnPoint.position, Quaternion.identity);
+        newObstacle.transform.parent = transform;
+        newObstacle.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5));
+        counter = 1f;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public bool power = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (power)
+        {
+            Color tmp = GetComponent<SpriteRenderer>().color;
+            tmp.a = 0.5f;
+            GetComponent<SpriteRenderer>().color = tmp;
+            Physics2D.IgnoreLayerCollision(9,8,true);
+        }
+        else
+        {
+            Color tmp = GetComponent<SpriteRenderer>().color;
+            tmp.a = 1f;
+            GetComponent<SpriteRenderer>().color = tmp;
+            Physics2D.IgnoreLayerCollision(9, 8, false);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Obstacle" && !power)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void isTouching(bool isActive)
+    {
+        power = isActive;
+    }
+}
